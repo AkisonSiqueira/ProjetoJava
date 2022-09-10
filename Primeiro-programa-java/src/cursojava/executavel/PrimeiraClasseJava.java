879864b2829faee7aef9628abdf5f7dd1d31c6cf
AKS <AKS@DESKTOP-G7HQ93K>
2022-09-10 10:54:23 -0300

    Criação do Projeto

---
+++ Primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,11 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+	
+	/*Main e um codigo executavel*/
+	
+ public static void main(String[] args) {
+	 System.out.println("Hello World");
+}
+ 
+}
